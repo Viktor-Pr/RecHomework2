7c25c18c8fe575fa5a011a82964015b46fdb7f59
Viktor-Pr <kolesovvb@gggmail.com>
2024-04-05 19:46:20 +0300

    Task3

---
+++ task2.3/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+int n = 45; 
+if (n / 10 > n % 10) 
+Console.WriteLine("First"); 
+else Console.WriteLine("second");
+
