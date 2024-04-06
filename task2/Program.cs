a997a1c3870b52538e8b97d1937d16d4e7e85d68
Viktor-Pr <kolesovvb@gggmail.com>
2024-04-06 13:39:39 +0300

    Задача 1 Два варианта ответа

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,9 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-// Console.WriteLine("Hello, World!");
-
+﻿
 // Задача 1. Напишите программу, которая принимает на вход число 
 // и проверяет, кратно ли оно одновременно 7 и 23.
-int n = 5278;
+int n = 755278;
 if (n % 7 == 0 & n % 23 == 0){
     Console.Write("Кратно");
 }
--- task2/obj/Debug/net8.0/task2.AssemblyInfo.cs
+++ task2/obj/Debug/net8.0/task2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("task2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f801f0d6f223c9992601d8b9fd8dc28e9ab8099")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+437ca7401a263cce5b4f8799977a5400a3e26a33")]
 [assembly: System.Reflection.AssemblyProductAttribute("task2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("task2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
